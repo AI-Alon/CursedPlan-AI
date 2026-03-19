b7b575aa40f3efe8fe0fe27505d7944ec04214db
AI-Alon <alon@cursedplan.ai>
2026-03-19 11:55:20 +0200

    rename public/ to docs/ for GitHub Pages

---
+++ src/js/firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDjB8It7AsphN5M9sa3h7M3UhhX1Dl__bU",
+  authDomain: "coursedplan.firebaseapp.com",
+  projectId: "coursedplan",
+  storageBucket: "coursedplan.firebasestorage.app",
+  messagingSenderId: "849767340088",
+  appId: "1:849767340088:web:b932eaba65c849b43beecd"
+};
